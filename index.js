b994f59d964051ccb06becdee2a3da1c1148ae9f
sydneyhornidge <1hornidgesyd@hdsb.ca>
2024-09-18 21:45:44 -0400

    made updates
    added user friendly text and comments throughout code

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
+// NAME: SYDNEY HORNIDGE
+// DATE: SEPTEMBER 18
+
+// Declared variables
+
 let x;
 let y;
 let operator;
 
+// Console text to guide the user
 
-x = parseFloat(prompt("First number:  "));
+console.log("-- Welcome to the Calculator! --")
+console.log("Please enter the two numbers with the operation you wish to perform")
+
+// Prompted user for numbers and an operation
+// Added Error Handling using if/else if/else Control Flow to stop the program if the operation is not valid
+
+x = parseFloat(prompt("Please Insert First number:  "));
 if (isNaN(x)) {
     console.log("Error! Enter valid number"); 
 }
 else {
-    operator = prompt("Operation:  ");
+    operator = prompt("Please Insert Operation:  ");
     if (operator != "+" && operator != "-" && operator != "x" && operator != "*" && operator != "/") {
         console.log("Invalid operation! Try again!");
     }
     else {
-        y = parseFloat(prompt("Second number:  "));
+        y = parseFloat(prompt("Please Insert Second number:  "));
         if (isNaN(y)) {
             console.log("Error! Enter valid number"); 
          }
      }
 }
 
+// Created Calculate Function
 
 calculate(x, operator, y); 
+
+// Added Calculate Function which can be called
+// Uses if/else if/else Control Flow to perform valid calculations, but stops if the operation is invalid
 
 function calculate(x, operator, y) {
     let result;
@@ -42,6 +58,10 @@ function calculate(x, operator, y) {
         result = ("Invalid operation! Try again!");
     }
     
-    console.log("Answer:  " + result);
+    console.log("The answer is:  " + result);
 
 }
+
+// Ending message
+
+console.log("Thank you for using the calculator!")
